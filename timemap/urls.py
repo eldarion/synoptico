d969d05f2839568f6fc85ec63097f77be5770b75
James Tauber <jtauber@jtauber.com>
2014-09-21 16:26:57 -0400

    URL for project detail

--- timemap/urls.py
+++ timemap/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 
 from .views import (
     ajax_autocomplete_events,
+    ProjectDetailView,
     TimelineDetailView
 )
 
@@ -17,6 +18,11 @@ urlpatterns = patterns(
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
 
+    url(
+        r"^projects/(?P<slug>[\w-]+)/$",
+        ProjectDetailView.as_view(),
+        name="project_detail"
+    ),
     url(
         r"^projects/(?P<slug>[\w-]+)/timelines/(?P<pk>\d+)/$",
         TimelineDetailView.as_view(),
